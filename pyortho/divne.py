2d0f4e8b3689fe83974593bd9f1b2cbcc5201bf9
Wei Chen <74291666+cwjycd@users.noreply.github.com>
2022-04-19 11:55:40 +0800

    Update divne.py

--- pyortho/divne.py
+++ pyortho/divne.py
@@ -20,7 +20,7 @@ def divne(num, den, Niter, rect, ndat, eps_dv, eps_cg, tol_cg,verb):
 	#rat: output ratio
 	# 
 	#Reference
-	#H. Wang, Y. Chen, O. Saad, W. Chen, Y. Oboue, L. Yang, S. Fomel, and Y. Chen, 2021, A Matlab code package for 2D/3D local slope estimation and structural filtering: in press.
+	#H. Wang, Y. Chen, O. Saad, W. Chen, Y. Oboue, L. Yang, S. Fomel, and Y. Chen, 2022, A Matlab code package for 2D/3D local slope estimation and structural filtering. Geophysics, doi: 10.1190/geo2021-0266.1.
 	n=num.size
 	
 	ifhasp0=0
